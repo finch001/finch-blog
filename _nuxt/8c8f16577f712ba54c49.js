b4500087e72dc74f38f0862f73c2589b72b117ec
finch001 <422465778@qq.com>
2020-07-02 06:06:37 +0000

    Updates

---
+++ _nuxt/8c8f16577f712ba54c49.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{116:function(e,n,o){"use strict";o.r(n),n.default={title:"Marina Aísa - Product Designer and Front-End Developer",description:"I’m a Product Designer (UI/UX) and Front-End Developer based in Barcelona."}}}]);
---
+++ _nuxt/fa430f3b6057f1711202.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{119:function(e,o){e.exports={I18N:{useCookie:!1,alwaysRedirect:!0,locales:[{code:"en",iso:"en-US",name:"English",file:"en/index.js"},{code:"es",iso:"es-ES",name:"Español",file:"es/index.js"}],lazy:!0,seo:!1,langDir:"/locales/",defaultLocale:"en",parsePages:!1}}}}]);
