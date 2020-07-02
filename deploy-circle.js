723eb44116eaab1d45035f2cb2b59422b4102532
mobai <mobai@douyu.tv>
2020-07-02 16:12:32 +0800

    feat: 默认分支变动

--- deploy-circle.js
+++ deploy-circle.js
@@ -1,14 +1,8 @@
 var ghpages = require("gh-pages");
 
-ghpages.publish(
-  "./dist",
-  {
-    branch: "master",
-  },
-  function(err) {
-    if (err) {
-      console.error(err);
-    }
-    console.log("deploy success");
+ghpages.publish("./dist", function (err) {
+  if (err) {
+    console.error(err);
   }
-);
+  console.log("deploy success");
+});
