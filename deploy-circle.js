4ea8630dd093dc2c789d0a722cdc40f85676a06f
mobai <mobai@douyu.tv>
2020-07-02 14:21:17 +0800

    feat: change branch

--- deploy-circle.js
+++ deploy-circle.js
@@ -1,8 +1,14 @@
 var ghpages = require("gh-pages");
 
-ghpages.publish("./dist", "master", function(err) {
-  if (err) {
-    console.error(err);
+ghpages.publish(
+  "./dist",
+  {
+    branch: "master",
+  },
+  function(err) {
+    if (err) {
+      console.error(err);
+    }
+    console.log("deploy success");
   }
-  console.log("deploy success");
-});
+);
